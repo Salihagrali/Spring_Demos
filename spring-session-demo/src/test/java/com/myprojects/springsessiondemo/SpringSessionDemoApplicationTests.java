b0fafeb5499eefd41e7addf3e016296415b6d7e2
Salih <imago_999@hotmail.com>
2025-02-11 22:45:51 +0300

    Spring session with redis in docker

---
+++ spring-session-demo/src/test/java/com/myprojects/springsessiondemo/SpringSessionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.springsessiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
